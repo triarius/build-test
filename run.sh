4e6403a5cb316953adb4607add6a3f89b15a41ba
Narthana Epa <narthana.epa@gmail.com>
2022-11-10 23:22:49 +1100

    Use OIDC plugin

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ buildkite-agent --version
 docker compose version
 
 buildkite-agent oidc token || true
-buildkite-agent oidc token --audience sts.amazonaws.com
+buildkite-agent oidc token --audience sts.amazonaws.com || true
+echo "$BUILDKITE_OIDC_TOKEN"
 
 echo "Hello From Nepa"
