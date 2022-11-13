5587655bc97a238ca879725c5e648cd032962723
Narthana Epa <narthana.epa@gmail.com>
2022-11-13 23:41:57 +1100

    Remove oidc token from plugin echo

--- run.sh
+++ run.sh
@@ -17,6 +17,4 @@ docker compose version
 buildkite-agent oidc request-token
 buildkite-agent oidc request-token --audience sts.amazonaws.com
 
-echo "$BUILDKITE_OIDC_TOKEN"
-
 echo "Hello From Nepa"
