41ea09fafbbabfb256805f4f05d8ea66de644580
Narthana Epa <narthana.epa@gmail.com>
2022-11-21 00:06:56 +1100

    Fix use of default import

--- .buildkite/pipeline.ts
+++ .buildkite/pipeline.ts
@@ -2,6 +2,8 @@ import { Pipeline, isPipeline } from "@triarius/pipeline-schema";
 import { parse } from "yaml";
 import { readFileSync, readdirSync } from "fs";
 
+const DIR = "dist";
+
 const pipeline_transformer_filename_pattern = /\.transformer\.js$/;
 const filename = "./pipeline.yaml";
 
@@ -9,18 +11,18 @@ async function import_and_transform(
   pipeline: Pipeline,
   name: string
 ): Promise<Pipeline> {
-  const transform = await import(name);
-  return transform(pipeline);
+  const transformer = await import(name);
+  return transformer.default(pipeline);
 }
 
 const pipeline: Pipeline = parse(readFileSync(filename, "utf-8"));
 if (!isPipeline(pipeline)) {
-  throw new Error(`{filename} is not a valid pipeline`);
+  throw new Error(`${filename} is not a valid pipeline`);
 }
 
-const files = readdirSync(".", { encoding: "utf-8" }).filter((f) =>
-  f.match(pipeline_transformer_filename_pattern)
-);
+const files = readdirSync(DIR, { encoding: "utf-8" })
+  .filter((f) => f.match(pipeline_transformer_filename_pattern))
+  .map((f) => `./${f}`);
 
 const finalPipeline = await files.reduce(async (pipeline, filename) => {
   const newPipeline = await import_and_transform(await pipeline, filename);
