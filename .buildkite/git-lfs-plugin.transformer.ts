c65a8d52645b842da549458222d1f46faa927380
Narthana Epa <narthana.epa@gmail.com>
2022-11-20 23:50:30 +1100

    Add git-lfs-plugin

---
+++ .buildkite/git-lfs-plugin.transformer.ts
@@ -0,0 +1,17 @@
+import { Pipeline, isCommandStep } from "@triarius/pipeline-schema";
+
+export default function (pipeline: Pipeline): Pipeline {
+  pipeline.steps = (pipeline.steps || []).map((step) =>
+    isCommandStep(step)
+      ? {
+          ...step,
+          plugins: {
+            ...(step.plugins || {}),
+            "triarius/git-lfs-buildkite-plugin:v0.1.0": {},
+          },
+        }
+      : step
+  );
+
+  return pipeline;
+}
