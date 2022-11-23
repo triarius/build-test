df1c4736da2f7107d852ffc2576f6f601b8a5bca
Narthana Epa <narthana.epa@gmail.com>
2022-11-23 21:48:38 +1100

    Upload File

---
+++ upload.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -Eeufxo pipefail
+
+echo Hello from Build "$BUILDKITE_BUILD_NUMBER" > hello.txt
+
+aws s3 cp hello.txt "s3://buildkite-nepa-test-oidc/${BUILDKITE_BUILD_NUMBER}/hello.txt"
