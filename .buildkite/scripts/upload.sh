60c20afe2ce62b31e810eef9d24d5d0cdb888891
Narthana Epa <narthana.epa@gmail.com>
2022-11-24 09:43:25 +1100

    Add a message

--- .buildkite/scripts/upload.sh
+++ .buildkite/scripts/upload.sh
@@ -4,5 +4,7 @@ set -Eeufo pipefail
 
 echo Hello from Build "$BUILDKITE_BUILD_NUMBER" > hello.txt
 
+echo --- :s3::arrow_up: Uploading to S3
+
 aws s3 cp hello.txt "s3://buildkite-nepa-test-oidc/${BUILDKITE_BUILD_NUMBER}/hello.txt"
 aws s3 cp hello.txt "s3://buildkite-nepa-test-oidc/${BUILDKITE_BRANCH}/hello.txt"
