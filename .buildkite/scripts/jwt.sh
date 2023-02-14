066dec9ddbb6d30ea35c8f023c9d22c2e194abab
Narthana Epa <narthana.epa@gmail.com>
2023-02-15 10:07:07 +1100

    Add jwt back

--- .buildkite/scripts/jwt.sh
+++ .buildkite/scripts/jwt.sh
@@ -10,13 +10,16 @@ failure() {
 
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
-# docker buildx build --progress=plain -t jwt:latest .
-#
-# function jwt {
-#   docker run --rm -i -e FORCE_COLOR=3 jwt:latest
-# }
+docker buildx build --progress=plain -t jwt:latest .
 
+function jwt {
+  docker run --rm -i -e FORCE_COLOR=3 jwt:latest
+}
 
 echo --- :key: OIDC Tokens
 
-buildkite-agent oidc request-token --claim organization_id --claim pipeline_id
+buildkite-agent oidc request-token | jwt
+buildkite-agent oidc request-token --audience sts.amazonaws.com | jwt
+buildkite-agent oidc request-token --audience sts.amazonaws.com --lifetime 0 | jwt
+buildkite-agent oidc request-token --audience sts.amazonaws.com --lifetime 600 | jwt
+buildkite-agent oidc request-token --claim organization_id --claim pipeline_id | jwt
