270120f14d5f9209672bbe335055cd0ee3ff76a0
Narthana Epa <narthana.epa@gmail.com>
2023-02-25 17:51:21 +1100

    Uninstall first

--- .buildkite/scripts/build.sh
+++ .buildkite/scripts/build.sh
@@ -2,9 +2,13 @@
 
 set -Eeufo pipefail
 
-docker run --privileged --userns=host --rm "tonistiigi/binfmt:qemu-v6.2.0" --install all
+QEMU_VERSION=6.2.0
+
+docker run --privileged --userns=host --rm "tonistiigi/binfmt:qemu-v$QEMU_VERSION" --uninstall qemu-*
+docker run --privileged --userns=host --rm "tonistiigi/binfmt:qemu-v$QEMU_VERSION" --install all
 
 builder_name=$(docker buildx create --use)
 # shellcheck disable=SC2064 # we want the current $builder_name to be trapped, not the runtime one
 trap "docker buildx rm $builder_name || true" EXIT
+
 docker buildx build --progress plain --builder "$builder_name" --platform linux/amd64,linux/arm64 .
