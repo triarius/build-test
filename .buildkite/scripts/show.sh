b0930846d261acbc63402c0c0e59d4b2875ebdab
Narthana Epa <narthana.epa@gmail.com>
2023-05-23 16:44:39 +1000

    Inline upload

--- .buildkite/scripts/show.sh
+++ .buildkite/scripts/show.sh
@@ -6,5 +6,12 @@ function inline_image {
   printf '\033]1338;url='"%s"';alt='"%s"'\a\n' "$1" "$2"
 }
 
+mkdir -p "artifacts/$BUILDKITE_BUILD_NUMBER"
+
+curl -sSL https://buildkite.com/_site/favicon.png > "artifacts/${BUILDKITE_BUILD_NUMBER}/logo.png"
+curl -sSL https://buildkite.com/_site/favicon.png > "artifacts/${BUILDKITE_BUILD_NUMBER}/logo 1.png"
+
+buildkite-agent artrifact upload 'artifacts/**/*.png'
+
 inline_image "artifact://artifacts/$BUILDKITE_BUILD_NUMBER/logo.png" 'OMG'
 inline_image "artifact://artifacts/$BUILDKITE_BUILD_NUMBER/logo 1.png" 'OMG'
