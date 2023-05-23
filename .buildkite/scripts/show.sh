512ab2c210e41726925a6891bf6e45ce526e2a7e
Narthana Epa <narthana.epa@gmail.com>
2023-05-23 16:37:27 +1000

    Show no spaces too

--- .buildkite/scripts/show.sh
+++ .buildkite/scripts/show.sh
@@ -6,4 +6,5 @@ function inline_image {
   printf '\033]1338;url='"%s"';alt='"%s"'\a\n' "$1" "$2"
 }
 
+inline_image 'artifact://artifacts/205/logo.png' 'OMG'
 inline_image 'artifact://artifacts/205/logo 1.png' 'OMG'
