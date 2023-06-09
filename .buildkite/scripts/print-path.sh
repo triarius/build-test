da60de802afae86f89bcaeff61dbd05444353752
Narthana Epa <narthana.epa@gmail.com>
2023-06-09 17:42:33 +1000

    Fix last pipe should be redirect

--- .buildkite/scripts/print-path.sh
+++ .buildkite/scripts/print-path.sh
@@ -3,4 +3,4 @@
 set -eufo pipefail
 
 mkdir -p artifacts
-IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done | sort -u | artifacts/path.txt
+IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done | sort -u > artifacts/path.txt
