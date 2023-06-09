81550f6cd7db223cce4fdfeddc6555046ece20da
Narthana Epa <narthana.epa@gmail.com>
2023-06-09 17:34:19 +1000

    Save paths as an artifact

--- .buildkite/scripts/print-path.sh
+++ .buildkite/scripts/print-path.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done
+mkdir -p artifacts
+IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done > artifacts/path.txt
