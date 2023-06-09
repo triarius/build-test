1c8032d93563541dac5125f55eb3c0e423cfd6c1
Narthana Epa <narthana.epa@gmail.com>
2023-06-09 17:41:49 +1000

    Set sets

--- .buildkite/scripts/print-path.sh
+++ .buildkite/scripts/print-path.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -eufo pipefail
+
 mkdir -p artifacts
 IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done | sort -u | artifacts/path.txt
