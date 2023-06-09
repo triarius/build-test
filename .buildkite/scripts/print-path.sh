40d1d32bbc78f38d8277cf8c3d7647cb0f479049
Narthana Epa <narthana.epa@gmail.com>
2023-06-09 17:35:22 +1000

    Sort and dedup

--- .buildkite/scripts/print-path.sh
+++ .buildkite/scripts/print-path.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p artifacts
-IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done > artifacts/path.txt
+IFS=':'; for i in $PATH; do test -d "$i" && find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done | sort -u | artifacts/path.txt
