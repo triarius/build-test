76c939b30146a6528acdd6b257e72a63739ceab2
Narthana Epa <narthana.epa@gmail.com>
2023-07-02 22:55:53 +1000

    Print process trees

--- .buildkite/scripts/sleep.sh
+++ .buildkite/scripts/sleep.sh
@@ -7,4 +7,13 @@ trap "echo sigterm received from sleep script" SIGTERM
 mkdir -p tests
 touch tests/test.txt
 
+echo pid: $$
+ps -eo pid,ppid,pgid,args | grep $$
+
+echo parent pid: $PPID
+ps -eo pid,ppid,pgid,args | grep $PPID
+
+echo printing process tree of grandparent
+pstree -hp "$(ps -o ppid= -p $PPID)"
+
 sleep 60
